6e6b47cf8595dd521d63ede4f9dd858e9c53a43b
fauzan_bintang <obin.aji@gmail.com>
2019-01-11 14:40:38 +0700

    add translateCon & googleLogin

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const userRouter = require('./users');
+const translateRouter = require('./translate');
 
 router.use('/users', userRouter);
+router.use('/translate', translateRouter);
 
 module.exports = router;
